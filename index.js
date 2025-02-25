00a69665139213b4cb9cb96ca57b5ce7f67d73fc
Peltre <pedripjs@gmail.com>
2025-02-25 10:56:15 -0600

    Second Commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import indexRoutes from "./routes/index.routes";
+import ItemRoutes from "/routes/items.routes.js"
 
 const app = express();
 
 app.use(indexRoutes);
+app.use(ItemRoutes);
 
 app.listen(5000);
 
---
+++ utils/sql.js
@@ -0,0 +1,16 @@
+const sql = require('mssql')
+
+const sqlConfig = {
+  database: process.env.DB_NAME,
+  server: process.env.DB_SERVER,
+  options: {
+    trustedConnection: true, // Set to true if using Windows Authentication
+    trustServerCertificate: true, // Set to true if using self-signed certificates
+  }
+}
+
+const sqlConnect = async () => {
+    return await sql.connect(sqlConfig)
+};
+
+export { sqlConnect, sql };
