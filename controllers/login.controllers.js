39cf6caab1be0a82bc83ab6b05e4d3dff8d80954
Pedro Sotelo <pedripjs@gmail.com>
2025-03-31 12:15:47 -0600

    Modified login controllers
    Verification is being made, using function declared in password utils

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,15 +1,27 @@
 import {sqlConnect, sql} from '../utils/sql.js';
+import { verifyPassword } from '../utils/passwordUtils.js';
 
 export const login = async (req, res) => {
-    const pool = await sqlConnect();
-    const data = await pool.request()
-    .input("userName",sql.VarChar,req.body.userName)
-    .query('select * from usuario where userName = @userName');
-    // console.log(data.recordset);
-    let isLogin = (data.recordset[0].passwordd === req.body.passwordd);
-    if(isLogin) {
-        res.status(200).json({ isLogin: isLogin, user: data.recordset[0] });
-    } else {
-        res.status(200).json({ isLogin: isLogin, user: {} });
+    try {
+        const pool = await sqlConnect();
+        const data = await pool.request()
+            .input("userName", sql.VarChar, req.body.userName)
+            .query('SELECT * FROM usuario WHERE userName = @userName');
+        
+        if (data.recordset.length === 0) {
+            return res.status(200).json({ isLogin: false, user: {} });
+        }
+
+        const user = data.recordset[0];
+        const isLogin = verifyPassword(req.body.password, user.passwordd, user.salt);
+        
+        if (isLogin) {
+            res.status(200).json({ isLogin: isLogin, user: user });
+        } else {
+            res.status(200).json({ isLogin: false, user: {} });
+        }
+    } catch (error) {
+        console.error('Error en login:', error);
+        res.status(500).json({ error: 'Error en el servidor' });
     }
 }
